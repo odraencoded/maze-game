6caab8ec94b4a3aaa3b80a8a31934ed674eb817f
Leonardo Augusto Pereira <odraencoded@gmail.com>
2014-07-04 18:09:07 -0300

    Added a player that moves

--- source/game.d
+++ source/game.d
@@ -1,6 +1,26 @@
 import dsfml.graphics;
 
+alias Point = Vector2!int;
+
 class Game {
 	RenderWindow window;
 	bool isRunning;
+	
+	Stage stage;
+}
+
+enum OnOffState {
+	Off = 0, On = 1,
+	Changed = 2,
+	
+	TurnedOff = Changed | Off,
+	TurnedOn = Changed | On
+}
+
+class Stage {
+	Pusher player;
+}
+
+class Pusher {
+	Point position;
 }
--- source/main.d
+++ source/main.d
@@ -1,4 +1,5 @@
 import dsfml.graphics;
+import std.stdio;
 import game;
 
 enum GAME_WIDTH = 320;
@@ -6,11 +7,36 @@ enum GAME_HEIGHT = 180;
 enum GAME_FRAMERATE = 30;
 enum GAME_TITLE = "Maze Game";
 
+enum GO_UP_KEY = Keyboard.Key.I;
+enum GO_RIGHT_KEY = Keyboard.Key.L;
+enum GO_DOWN_KEY = Keyboard.Key.K;
+enum GO_LEFT_KEY = Keyboard.Key.J;
+
+enum BLOCK_SIZE = 16;
+
 void main(string[] args) {
 	Game game = new Game();
 	
 	// Open Window
 	auto window = game.window = setupWindow();
+	
+	// Create test stage
+	auto stage = game.stage = setupTestStage();
+	auto player = stage.player;
+	
+	// Setup input
+	OnOffState[int] input;
+	input[GO_UP_KEY] = OnOffState.Off;
+	input[GO_RIGHT_KEY] = OnOffState.Off;
+	input[GO_DOWN_KEY] = OnOffState.Off;
+	input[GO_LEFT_KEY] = OnOffState.Off;
+	
+	// Setup sprites
+	VertexArray playerSprite = new VertexArray(PrimitiveType.Triangles, 3);
+	playerSprite[0].position = Vector2f(2, 2);
+	playerSprite[1].position = Vector2f(14, 2);
+	playerSprite[2].position = Vector2f(8, 12);
+	for(int i=0; i<3; i++) playerSprite[i].color = Color.White;
 	
 	// Main loop
 	game.isRunning = true;
@@ -18,11 +44,35 @@ void main(string[] args) {
 		// Fixed delta
 		enum frameDelta = 1.0 / GAME_FRAMERATE;
 		
+		// Updating input register
+		foreach(int key, OnOffState value; input) {
+			input[key] = value & ~OnOffState.Changed;
+		}
+		
 		// Polling events
 		Event event;
 		while(window.pollEvent(event)) {
-			if(event.type == event.EventType.Closed)
-				window.close();
+			switch(event.type) {
+				// Close window
+				case(event.EventType.Closed):
+					window.close();
+					break;
+				
+				// Register input
+				case(event.EventType.KeyPressed):
+					auto code = event.key.code;
+					if(code in input)
+						input[code] = OnOffState.TurnedOn;
+					break;
+				
+				case(event.EventType.KeyReleased):
+					auto code = event.key.code;
+					if(code in input)
+						input[code] = OnOffState.TurnedOff;
+					break;
+				
+				default:
+			}
 		}
 		
 		// Exiting loop
@@ -30,8 +80,32 @@ void main(string[] args) {
 		if(!game.isRunning)
 			break;
 		
+		// Move player
+		Point movement;
+		if(input[GO_UP_KEY] == OnOffState.TurnedOn)
+			movement.y -= 1;
+		if(input[GO_DOWN_KEY] == OnOffState.TurnedOn)
+			movement.y += 1;
+		if(input[GO_LEFT_KEY] == OnOffState.TurnedOn)
+			movement.x -= 1;
+		if(input[GO_RIGHT_KEY] == OnOffState.TurnedOn)
+			movement.x += 1;
+		
+		player.position += movement;
+		
 		// Draw stuff
 		window.clear();
+		
+		// Draw player
+		RenderStates state;
+		state.transform = Transform.Identity;
+		
+		state.transform.translate(
+			player.position.x * BLOCK_SIZE,
+			player.position.y * BLOCK_SIZE
+		);
+		
+		window.draw(playerSprite, state);
 		window.display();
 	}
 }
@@ -41,4 +115,10 @@ private RenderWindow setupWindow() {
 	auto window = new RenderWindow(videoMode, GAME_TITLE);
 	window.setFramerateLimit(GAME_FRAMERATE);
 	return window;
+}
+
+private Stage setupTestStage() {
+	auto stage = new Stage();
+	stage.player = new Pusher();
+	return stage;
 }
